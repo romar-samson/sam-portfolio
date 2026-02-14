04ac05a1db7388b57baebd347e8f16090a3ca3e3
Uqlzsam <161308576+Uqlzsam@users.noreply.github.com>
2026-02-15 02:18:08 +0800

    Done with adding background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Threads from "./components/Threads"
+
 function App() {
   return (
     <>
-      
+      <Threads
+        className="fixed inset-0 w-full h-full -z-10"
+        color={[1, 1, 1]}
+        amplitude={1}
+        distance={0}
+        enableMouseInteraction
+      />
     </>
   )
 }
